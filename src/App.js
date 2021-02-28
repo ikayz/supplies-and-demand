03a7b8a4d3557fc45fb1882c91c09ee46277b4e7
Isaac Miti <ikayz@live.com>
2021-02-28 12:29:49 +0200

    Add sign up form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import "./App.css";
 import Home from "./components/Home";
+import Form from "./components/Form";
 
 function App() {
   return (
     <Fragment>
-      <Home />
+      {/* <Home /> */}
+      <Form />
     </Fragment>
   );
 }
